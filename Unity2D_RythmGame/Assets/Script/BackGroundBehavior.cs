0aeadb9950171ed3a71047d405fd20050ddbfaa8
EZHoon93 <47181408+EZHoon93@users.noreply.github.com>
2019-09-18 13:08:27 +0900

    3-2 판정선과 구성까지

---
+++ Unity2D_RythmGame/Assets/Script/BackGroundBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundBehavior : MonoBehaviour
+{
+
+    public GameObject gameBackground;
+    private SpriteRenderer gameBackgroundSrpiteRender;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameBackgroundSrpiteRender = gameBackground.GetComponent<SpriteRenderer>();
+        StartCoroutine(FadeOut(gameBackgroundSrpiteRender,0.005f)  );
+    }
+
+    IEnumerator FadeOut(SpriteRenderer spriteRenderer, float amount)
+    {
+        Color color = spriteRenderer.color;
+
+        while (color.a > 0.0f)
+        {
+            color.a -= amount;
+            spriteRenderer.color = color;
+            yield return new WaitForSeconds(amount);
+        }
+    }
+}
